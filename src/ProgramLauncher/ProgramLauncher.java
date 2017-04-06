7c13f4e21f431c40175d0683c1790a7db07cca8c
xShaharX <shahar0897@gmail.com>
2017-04-06 15:21:37 +0300

    added more classes

--- src/ProgramLauncher/ProgramLauncher.java
+++ src/ProgramLauncher/ProgramLauncher.java
@@ -1,5 +1,9 @@
 package ProgramLauncher;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 /**
  * Created by Shahar on 06/04/17.
  */
@@ -7,6 +11,33 @@ public class ProgramLauncher
 {
     public static void main(String [] args)
     {
+        //TODO
 
+    }
+
+    public static Connection getConnectionAndInitDatabase(String dataBaseName)
+    {
+        Connection c = null;
+        Statement stmt = null;
+        try {
+            /*Opening Connection*/
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:"+dataBaseName);
+
+            c.createStatement().execute("PRAGMA FOREIGN_KEYS = ON;");
+
+            /*Creating Tables if they are NOT existed */
+
+            //TODO
+
+
+            stmt.close();
+
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+
+        return c;
     }
 }
